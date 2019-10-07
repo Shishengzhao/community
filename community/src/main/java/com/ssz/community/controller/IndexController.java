a45d69edda6bf5a10ad2759321c09c8600dd2dbd
ssz <1779326001@qq.com>
2019-10-07 16:44:03 +0800

    add index and bootstrap

--- community/src/main/java/com/ssz/community/controller/IndexController.java
+++ community/src/main/java/com/ssz/community/controller/IndexController.java
@@ -6,13 +6,10 @@ package com.ssz.community.controller;
         import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class HelloController {
+public class IndexController {
 
-    @GetMapping("/hello")
-    public String hello(@RequestParam(name = "name") String name,
-                        Model model){
-        model.addAttribute("name",name);
-        return "hello";
-
+    @GetMapping("/")
+    public String hello(){
+        return "index";
     }
 }
